a6dc4312f895e7e55a8e9b8c5db6ca1ea33e22e4
Abdul Ahad <abdul.ahadkh23@gmail.com>
2026-01-06 17:20:23 +0530

    Finalize k3s setup, Open WebUI deployment, and OIDC analysis

--- k3s/install.sh
+++ k3s/install.sh
@@ -1,1 +1,18 @@
 # k3s/install.sh
+#!/bin/bash
+set -e
+
+# Install Docker
+apt update
+apt install -y ca-certificates curl gnupg
+
+curl -fsSL https://get.docker.com | sh
+usermod -aG docker $SUDO_USER
+
+# Install k3s
+curl -sfL https://get.k3s.io | sh -
+
+# Configure kubeconfig
+mkdir -p /home/$SUDO_USER/.kube
+cp /etc/rancher/k3s/k3s.yaml /home/$SUDO_USER/.kube/config
+chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.kube/config
